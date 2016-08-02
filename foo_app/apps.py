9218367cf99e101d6e47c46c4b8d0f7c45defae4
Sergey Urchenko <Sergey.Urchenko@msk.action.ru>
2016-08-02 19:15:25 +0300

    default_permissions bug

---
+++ foo_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class FooAppConfig(AppConfig):
+    name = 'foo_app'
---
+++ foo_app/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+class Foo(models.Model):
+    pass
+
+    class Meta:
+        default_permissions = ()
